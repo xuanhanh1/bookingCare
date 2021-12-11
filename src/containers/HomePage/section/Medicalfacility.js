a01184e60ea670148df7536668ce8b7127b3e82b
superthaihoa123 <superthaihoa123@gmail.com>
2021-12-11 14:52:51 +0700

    Medicalfacility

--- src/containers/HomePage/section/Medicalfacility.js
+++ src/containers/HomePage/section/Medicalfacility.js
@@ -13,43 +13,94 @@ class Medicalfacility extends Component {
             infinite: false,
             speed: 500,
             slidesToShow: 4,
-            slidesToScroll: 1
+            slidesToScroll: 4,
+            initialSlide: 0,
+            responsive: [
+
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         };
         return (
-            <section className="Medicalfacility sections">
-                <div className="sections-header">
+            <div className="specialty container section">
+                {/* <h1 className="section_heading">Chuyên khoa phổ biến</h1> */}
+                <div className="section__header">
                     <h3>Cơ sở y tế nổi bật</h3>
                     <button>Xem Thêm</button>
                 </div>
-                <div className="sections-content">
-                    <Slider {...settings}>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Cơ sở Thu Cúc</span>
+                <Slider {...settings}>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Cơ sở quận 3</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Cơ sở Thu Cúc</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Cơ sở quận 3</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Cơ sở Thu Cúc</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Cơ sở quận 3</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Cơ sở Thu Cúc</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Cơ sở Bình Thạnh</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Cơ sở Thu Cúc</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Cơ sở quận 7</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Cơ sở Thu Cúc</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Cơ sở quận 5</h5>
+                            </div>
                         </div>
-                    </Slider>
-                </div>
-            </section>
+                    </div>
+                </Slider>
+            </div >
         )
     }
 }
