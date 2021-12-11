5e7d4765d2899018d212b84443ae6c6e95f196bc
superthaihoa123 <superthaihoa123@gmail.com>
2021-12-11 14:46:35 +0700

    Specialty

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import Navbar from './HeaderHome/Navbar'
 import Specialty from './section/Specialty';
 import Medicalfacility from './section/Medicalfacility';
 import Doctor from './section/Doctor';
+import "./HomePage.scss"
 
 export class HomePage extends Component {
     render() {
--- src/containers/HomePage/section/Specialty.js
+++ src/containers/HomePage/section/Specialty.js
@@ -12,43 +12,94 @@ class Specialty extends Component {
             infinite: false,
             speed: 500,
             slidesToShow: 4,
-            slidesToScroll: 1
+            slidesToScroll: 4,
+            initialSlide: 0,
+            responsive: [
+
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         };
         return (
-            <section className="specialty sections">
-                <div className="sections-header">
+            <div className="specialty container section">
+                {/* <h1 className="section_heading">Chuyên khoa phổ biến</h1> */}
+                <div className="section__header">
                     <h3>Chuyên khoa phổ biến</h3>
                     <button>Xem Thêm</button>
                 </div>
-                <div className="sections-content">
-                    <Slider {...settings}>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Co xuong khop</span>
+                <Slider {...settings}>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Răng hàm mặt</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Co xuong khop</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Xương khớp</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Co xuong khop</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Tim mạnh</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Co xuong khop</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Chấn thương chỉnh hình</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Co xuong khop</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Nội soi</h5>
+                            </div>
                         </div>
-                        <div className="sections-content-img">
-                            <img src={specialtyimg}></img>
-                            <span>Co xuong khop</span>
+                    </div>
+                    <div className="specialty-card">
+                        <div className="card">
+                            <div className="card-img-top card-img" style={{ backgroundImage: `url(${specialtyimg})` }}></div>
+                            <div className="card-body">
+                                <h5 className="card-title">Tiêu hoá</h5>
+                            </div>
                         </div>
-                    </Slider>
-                </div>
-            </section>
+                    </div>
+                </Slider>
+            </div >
         )
     }
 }
