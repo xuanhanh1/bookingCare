ac756d784636c1cc513808cccc88fb2470c6a79d
superthaihoa123 <superthaihoa123@gmail.com>
2022-01-02 21:08:43 +0700

    display list specialty

--- src/containers/HomePage/HeaderHome/SectionNavBar/ListSpecialty.js
+++ src/containers/HomePage/HeaderHome/SectionNavBar/ListSpecialty.js
@@ -40,7 +40,22 @@ class ListSpecialty extends Component {
                     <div className="section__header title">
                         <h3>Danh sách chuyên khoa</h3>
                     </div>
-
+                    {listDoctors.map((item, index) => {
+                        let imageBase64 = '';
+                        if (item.image) {
+                            imageBase64 = new Buffer(item.image, 'base64').toString('binary');
+                        }
+                        let name = `${item.lastName} ${item.firstName}`
+                        console.log(item)
+                        return (
+                            <div className="sections-content list-nav"
+                                onClick={() => { this.handerViewDetailDoctor(item.id) }}
+                            >
+                                < img className='avatar' src={imageBase64} alt="" />
+                                <p className='name'> Bác sĩ {name}</p>
+                            </div>
+                        )
+                    })}
 
                 </section >
             </>
